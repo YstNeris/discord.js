ed03754b4dfabe5a5f71658e54a46076dfcd2587
YstNeris <ystneris@gmail.com>
2021-09-23 21:02:56 +0500

    refactor(Collection): add sorted method

--- src/util/Collection.js
+++ src/util/Collection.js
@@ -66,11 +66,29 @@ class Collection extends DDOCollection {
   }
 
   /**
+   * The sorted method sorts the items of a collection and returns it.
+   * The sort is not necessarily stable in Node 10 or older.
+   * The default sort order is according to string Unicode code points.
+   * @param {Function} [compareFunction] Specifies a function that defines the sort order.
+   * If omitted, the collection is sorted according to each character's Unicode code point value,
+   * according to the string conversion of each element.
+   * @returns {Collection}
+   * @example collection.sorted((userA, userB) => userA.createdTimestamp - userB.createdTimestamp);
+   */
+  sorted(compareFunction = Collection.defaultSort) {
+    return new this.constructor[Symbol.species](this).sort((av, bv, ak, bk) => compareFunction(av, bv, ak, bk));
+  }
+
+  /**
    * Collection toJSON is called recursively by JSON.stringify
    * @returns {[*]}
    */
   toJSON() {
     return [...this.values()];
+  }
+
+  static defaultSort(firstValue, secondValue) {
+    return Number(firstValue > secondValue) || Number(firstValue === secondValue) - 1;
   }
 }
 
